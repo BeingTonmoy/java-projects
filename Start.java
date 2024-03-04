90f70cf69fb93d9e6a206bd7bfdbaf3c2234b9c1
Arfan Rahman Tonmoy <143004660+BeingTonmoy@users.noreply.github.com>
2024-03-04 21:21:31 +0600

    Update Start.java

---
+++ Start.java
@@ -0,0 +1,9 @@
+package Frontend;
+
+public class Start {
+    public static void main (String[]args){
+       Programmer obj1 = new Programmer("Arfan", "arfanrahman12@gmail.com", 51598, "Tonmoy");
+       obj1.viewdetails();
+    }
+}
+
